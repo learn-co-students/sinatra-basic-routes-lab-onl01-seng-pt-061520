06c2a3f4464a431f2d0e0aeb01994a2da45111a5
Teodoro Garcia <teodorogarcia@Teodoros-MacBook-Air.local>
2020-09-14 20:52:58 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        "Hello World!"
+    end
+
+    get '/name' do 
+        "My name is _____"
+    end
+    
+    get '/hometown' do 
+        "My hometown is ______"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is _______"
+    end
+
 end
